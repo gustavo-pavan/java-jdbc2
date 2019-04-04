7e9cf4e1731cfbfc3694003010911c0ee10ea70b
Gustavo Pavan <gustavopavang@gmail.com>
2019-04-04 17:42:29 -0300

    Connection OK

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import java.sql.Connection;
+
+import db.DB;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Connection conn =  DB.getConnetion();
+		DB.closeConnetion();
 
 	}
 
