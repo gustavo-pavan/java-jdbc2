63d6bf123a71e111f1e5f1d82634956dd1c96a42
Gustavo Pavan <gustavopavang@gmail.com>
2019-04-04 18:03:24 -0300

    Read Data OK

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,38 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbExcepetion;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn =  DB.getConnetion();
-		DB.closeConnetion();
+		Connection conn = null;  // criar connexao
+		Statement st = null;	 // criar query
+		ResultSet rs = null; 	 // guardar resultado do banco
 
+		try {
+			conn = DB.getConnetion(); 						  // abrindo conexao com banco
+			st = conn.createStatement(); 					  //criando um statement
+			rs = st.executeQuery("select * from department"); // executando query
+			
+			while(rs.next()) // percorrer até não conter mais dados
+			{
+				System.out.println(rs.getInt("Id") + ". " + rs.getString("Name")); //imprimir dados na tela
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnetion();
+		}
 	}
 
 }
